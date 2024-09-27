9889630e21e779279ef5791c4298158d42954cdd
unknown <genuinedistinctclearcapital@gmail.com>
2024-09-27 12:07:10 -0300

    add a giant moving picture of a cat

--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,44 @@
-function love.load
-    image = love.graphics.newImage(res/ball.jpg)
-    love.graphics.setNewFont(12)
-    love.graphics.setColor(0,0,0)
-    love.graphics.setBackgroundColor(255,255,255)
-end;
-
-function love.update()
-
-end;
-
-function love.draw()
+function love.load()
+  
+    love.window.setTitle("cat")
     
-end;
+    love.graphics.setBackgroundColor(1, 1, 1)
+    ball = love.graphics.newImage("res/cat.jpg")
+    
+    posX = 0
+    posY = 0
+    angle = 0
+  
+    speed = 100.0
+  
+  end
+  
+  
+  function love.update(dt)
+    
+    if love.keyboard.isDown("right") then
+      posX = posX + speed * dt
+    end
+    
+    if love.keyboard.isDown("left") then
+      posX = posX - speed * dt
+    end
+    
+    if love.keyboard.isDown("up") then
+      posY = posY - speed * dt
+    end
+    
+    if love.keyboard.isDown("down") then
+      posY = posY + speed * dt
+    end
+    
+  end
+  
+  
+  function love.draw()
+    
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw  (ball, posX, posY, angle)
+  
+  end
+  
