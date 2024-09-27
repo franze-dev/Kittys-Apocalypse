ed05565c6a578db1f4bd8ae48a78958eb6111c62
unknown <genuinedistinctclearcapital@gmail.com>
2024-09-27 11:35:40 -0300

    first love test

---
+++ src/main.lua
@@ -0,0 +1,14 @@
+function love.load
+    image = love.graphics.newImage(res/ball.jpg)
+    love.graphics.setNewFont(12)
+    love.graphics.setColor(0,0,0)
+    love.graphics.setBackgroundColor(255,255,255)
+end;
+
+function love.update()
+
+end;
+
+function love.draw()
+    
+end;
