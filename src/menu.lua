27de056c86b32a96c44fc8fc2bb5f84ea8e13bef
aeffggbh <genuinedistinctclearcapital@gmail.com>
2024-10-02 13:04:10 -0300

    add scene switching to menu

--- src/menu.lua
+++ src/menu.lua
@@ -1,4 +1,5 @@
 require "uimanager"
+require "scenemanager"
 
 function Menu_Init()
    
@@ -8,7 +9,7 @@ function Menu_Init()
     titleColor1 = {
         r = 1.0,
         g = 0.5,
-        b = 0-0,
+        b = 0.0,
         a = 1.0
     }
 
@@ -30,15 +31,38 @@ function Menu_Init()
     title2.x = title2.x - title2.love:getWidth()/2
     title2.y = title1.y + title2.love:getHeight()/2
 
+    buttonsPadding = 40
+
+    generalTextColor = {
+        r = 1.0,
+        g = 1.0,
+        b = 1.0,
+        a = 1.0
+    }
+
+    playButton = newButton("PLAY", titleFont2, title2.x, title2.y + title2.love:getHeight() + buttonsPadding, title2.love:getWidth(), 40, titleColor1, titleColor2, generalTextColor)
+    creditsButton = newButton("CREDITS", titleFont1, playButton.x, playButton.y + buttonsPadding*2, playButton.width, playButton.height, titleColor1, titleColor2, generalTextColor)
+    exitButton = newButton("EXIT", titleFont1, creditsButton.x, creditsButton.y + buttonsPadding*2, creditsButton.width, creditsButton.height, creditsButton.default, creditsButton.highLight, generalTextColor)
+
 end
 
 function Menu_Update()
-    
+
+    checkSceneChange(playButton, scenes.gamePlayScene)
+
+    checkSceneChange(creditsButton, scenes.creditsScene)
+
+    checkSceneChange(exitButton, scenes.overScene)
+
 end
 
 function Menu_Draw()
   
     drawText(titleFont1, title1)
     drawText(titleFont2, title2)
+    
+    drawButton(titleFont2, playButton)
+    drawButton(titleFont1, creditsButton)
+    drawButton(titleFont1, exitButton)
 
 end
